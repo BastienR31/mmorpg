b41a51f8f116d84b64b1b91db0f5889832776aad
BastienR31 <bastien.ruchon81@gmail.com>
2020-04-26 18:27:55 +0200

    Add character animation, move character, inventory, bot and soundtrack

--- main.js
+++ main.js
@@ -38,53 +38,283 @@ require([
         dir: url.split("/").slice(0, -1) || "."
     };
 
-    // var myGameArea = {
-    //     canvas : document.createElement("canvas"),
-    //     start : function() {
-    //       this.canvas.width = 480;
-    //       this.canvas.height = 270;
-    //       this.context = this.canvas.getContext("2d");
-    //       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    //       this.interval = setInterval(updateGameArea, 20);
-    //       window.addEventListener('keydown', function (e) {
-    //         myGameArea.key = e.keyCode;
-    //       })
-    //       window.addEventListener('keyup', function (e) {
-    //         myGameArea.key = false;
-    //       })
-    //     },
-    //     clear : function(){
-    //       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    //     }
-    //   }
+    let worldmap = document.getElementById("map");
 
     let topChar = 320;
     let leftChar = 544;
+    let positionInitLeft = -8;
+    let positionInitRight = 114;
+
+    let maxHealth = 100;
+    let currentHealth = 100;
+
+    let isInventoryDisplayed = false;
+
+    let characterSprite = document.createElement("div");
+    characterSprite.setAttribute("id", 'characterSprite');
+    characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+
+    const playSound = () => {
+
+        let urlMusic = "/examples/sound/desert_ambient_1.mp3";
+        let sound = document.createElement("audio");
+
+        sound.src = urlMusic;
+
+        sound.volume = 0.01;
+        sound.play();
+    };
+
+    const playEffect = (effect) => {
+
+        console.log(effect, "melee_sound")
+
+        let urlEffect = `/examples/sound/effects/${effect}.wav`;
+        let audioEffect = document.createElement("audio");
+
+        audioEffect.src = urlEffect;
+
+        audioEffect.volume = 0.3;
+        audioEffect.play();
+    }
 
     //Move a character
     const moveCharacter = event => {
-        console.log(event)
-        console.log(event.key)
+
+        //Take damage method
+        //Si leftChar est entre -42 leftBot et + 42 && topChar est entre - 32 et +32 topBot
+
+        if (leftChar >= leftBot - 42 && leftChar <= leftBot + 42 && topChar >= topBot - 32 && topChar <= topBot + 32) {
+            console.log('take damage')
+            playEffect("melee_sound");
+            currentHealth -= 1;
+            displayLifeBar(currentHealth);
+        }
 
         //Up
         if (event.key === "ArrowUp") {
-            topChar -= 1;
-            characterRect.setAttribute("style", U.format("left: {0}px; top: {1}px;", leftChar, topChar));
+            topChar -= 3;
+            positionInitRight = 241;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
         }
 
         //Down
         if (event.key === "ArrowDown") {
-            topChar += 1;
-            characterRect.setAttribute("style", U.format("left: {0}px; top: {1}px;", leftChar, topChar));
+            topChar += 3;
+            positionInitRight = 114;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
         }
 
         //Left
-        if (event.key === "ArrowLeft") { }
+        if (event.key === "ArrowLeft") {
+            leftChar -= 3;
+            positionInitRight = 177;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
 
         //Right
-        if (event.key === "ArrowRight") { }
+        if (event.key === "ArrowRight") {
+            leftChar += 3;
+            positionInitRight = 49;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
     };
-    document.addEventListener("keydown", moveCharacter);
+
+    const displayLifeBar = (currentHealth) => {
+
+        let checkLifeBar = document.getElementById("lifeBar");
+
+        if (checkLifeBar !== null) {
+            worldmap.removeChild(checkLifeBar);
+        }
+
+        let lifeBar = document.createElement("div");
+        lifeBar.setAttribute("id", 'lifeBar');
+
+        lifeBar.setAttribute("style", "width: 150px; height: 25px; position: absolute; z-index: 999; top: 30px; left: 30px; border: solid 1px #aaa;");
+
+        let lifeCount = document.createElement("div");
+        let lifeCountText = document.createTextNode(currentHealth + "%");
+
+        lifeCount.appendChild(lifeCountText);
+        lifeCount.setAttribute("style", `color: black; width: ${currentHealth}%; background-color: red; display: flex; align-items: center; justify-content: center; height: 100%;`);
+
+        lifeBar.appendChild(lifeCount);
+
+        worldmap.appendChild(lifeBar);
+    };
+
+    const items = () => {
+        let potions = "examples/items/potions.png";
+
+        /*let displayPotions = document.createElement("div");
+        displayPotions.setAttribute("id", "potions");
+        displayPotions.setAttribute("style", `background-image: ${potions}; height: 24px; width: 24px;`);*/
+
+        //let displayPotions = document.createElement('div');
+        //displayPotions.innerHTML = '<div><a href="#"></a><span></span></div>';
+
+        console.log(potions)
+
+        const allItems = [
+            {
+                potions: [
+                    {
+                        url: 'examples/items/potions.png',
+                        idItem: 1,
+                        currentHealth: 50
+                    }
+                ]
+            }
+        ];
+
+        return [potions, { idItem: 1 }];
+    };
+
+    const useItems = item => {
+        console.log(item)
+        if (item.idItem === 1) {
+            currentHealth += 50;
+            if (currentHealth >= maxHealth) {
+                currentHealth = 100;
+            }
+            displayLifeBar(currentHealth);
+        }
+    };
+
+    const displayInventory = (event) => {
+        console.log(event)
+        console.log(event.key)
+
+        console.log(isInventoryDisplayed)
+
+        if (event.code === "KeyI" && isInventoryDisplayed === true) {
+
+            console.log(isInventoryDisplayed)
+
+            let getInventory = document.getElementById("inventory");
+
+            let worldmap = document.getElementById("map");
+
+            worldmap.removeChild(getInventory);
+
+            isInventoryDisplayed = false;
+        }
+        else if (event.code === "KeyI" && isInventoryDisplayed === false) {
+            console.log("display inventory")
+
+            isInventoryDisplayed = true;
+
+            let inventory = document.createElement("div");
+            inventory.setAttribute("id", "inventory");
+            inventory.setAttribute("style", "width: 250px; height: 200px; border: 1px solid rgba(95, 73, 46, .7); border-radius: 6px; background-color: #856D4D; opacity: 0.7;z-index: 2; position: absolute; top: 100px; left: 450px; display: flex; align-items: flex-end;");
+
+            const arrayInventory1 = ["cell1", "cell2", "cell3", "cell4"];
+            const arrayInventory2 = ["cell5", "cell6", "cell7", "cell8"];
+
+            let inventoryTable = document.createElement("table");
+            inventoryTable.setAttribute("style", "width: 100%; height: 50%; border: 1px solid black;")
+            let inventoryRow1 = document.createElement("tr");
+            let inventoryRow2 = document.createElement("tr");
+            //let inventoryCell = document.createElement("td");
+
+            const potions = items();
+
+            arrayInventory1.forEach(
+                (arrayInventoryItem, index) => {
+                    if (arrayInventoryItem !== undefined) {
+                        let inventoryCell = document.createElement("td");
+                        inventoryCell.setAttribute("id", `${arrayInventoryItem}`);
+                        inventoryCell.setAttribute("style", "height: 50px; width: 50px; border: 1px solid black;");
+                        inventoryCell.addEventListener("click", () => alert(`test ${arrayInventoryItem}`));
+
+                        if (index === 0) {
+
+                            let wrapper = document.createElement('td');
+                            wrapper.setAttribute("id", `${arrayInventoryItem}`);
+                            wrapper.setAttribute("style", "height: 50px; width: 50px; border: 1px solid black;");
+
+                            wrapper.addEventListener(
+                                "click", () => {
+                                    useItems(potions[1]);
+                                    let cell1 = document.getElementById("cell1");
+                                    cell1.removeChild(cell1.firstChild);
+                                }
+                            );
+                            wrapper.innerHTML = `<div style="background: url(${potions[0]}); width: 24px; height: 24px;"></div>`;
+
+                            console.log(potions)
+                            inventoryCell = wrapper;
+                            console.log(inventoryCell)
+                        }
+
+                        inventoryRow1.appendChild(inventoryCell);
+                    }
+                }
+            );
+            arrayInventory2.forEach(
+                arrayInventoryItem => {
+                    if (arrayInventoryItem !== undefined) {
+                        let inventoryCell = document.createElement("td");
+                        inventoryCell.setAttribute("id", `${arrayInventoryItem}`);
+                        inventoryCell.setAttribute("style", "height: 50px; width: 50px; border: 1px solid black;");
+                        inventoryRow2.appendChild(inventoryCell);
+                    }
+                }
+            );
+
+            inventoryTable.appendChild(inventoryRow1);
+            inventoryTable.appendChild(inventoryRow2);
+
+            inventory.appendChild(inventoryTable);
+
+            worldmap.appendChild(inventory);
+        }
+    };
+
+    let cpt = 0;
+
+    document.addEventListener("keydown", event => {
+
+        displayInventory(event);
+
+        if (cpt === 1) {
+            positionInitLeft = -70;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
+        if (cpt === 2) {
+            positionInitLeft = -134;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
+        if (cpt === 3) {
+            positionInitLeft = -198;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
+        if (cpt === 4) {
+            positionInitLeft = -262;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
+        if (cpt === 5) {
+            positionInitLeft = -326;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
+        if (cpt === 6) {
+            positionInitLeft = -390;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
+        if (cpt === 7) {
+            positionInitLeft = -454;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+        }
+        if (cpt === 8) {
+            positionInitLeft = -8;
+            characterSprite.setAttribute("style", U.format("left: {0}px; top: {1}px; background-position: {2}px {3}px;", leftChar, topChar, positionInitLeft, positionInitRight));
+            cpt = 0;
+        }
+        cpt++;
+        setInterval(moveCharacter(event), 4000);
+    });
+
     //Pure js (ECMA6)
     //Display the map
     fetch(url)
@@ -98,6 +328,8 @@ require([
 
                 Map.fromXML(doc, options).then(
                     map => {
+
+                        playSound();
 
                         document.getElementById("map").setAttribute("width", `${map.bounds.w * map.tileInfo.w}`);
                         document.getElementById("map").setAttribute("height", `${map.bounds.h * map.tileInfo.h}`);
@@ -219,19 +451,15 @@ require([
             alert(`${error} - Failed to open TMX file.`);
         });
 
-    let worldmap = document.getElementById("map");
+    //items();
+    displayLifeBar(currentHealth);
 
-    let characterRect = document.createElement("div");
-    characterRect.setAttribute("id", 'characterRect');
-    characterRect.setAttribute("class", "characterRectClass");
+    let botCombat = document.createElement("div");
+    let topBot = 240;
+    let leftBot = 784;
+    botCombat.setAttribute("id", 'fightBot');
+    botCombat.setAttribute("style", U.format("left: {0}px; top: {1}px;", leftBot, topBot));
 
-    characterRect.setAttribute("style", U.format("left: {0}px; top: {1}px;", leftChar, topChar));
-
-    //characterRect.setAttribute("style", "left: 544px");
-    //characterRect.setAttribute("style", "top: 320px");
-
-    worldmap.appendChild(characterRect);
-
-
-
+    worldmap.appendChild(botCombat);
+    worldmap.appendChild(characterSprite);
 });
